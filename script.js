1226e0db0eb407370739355229ca62ff84b68b79
Sakshi Sharma <sakshi@agencybrands.ca>
2025-11-25 15:43:03 -0500

    Initial commit: Tableau extension dev files

---
+++ script.js
@@ -0,0 +1,7 @@
+﻿document.getElementById('generate-insights').addEventListener('click', function() {
+    var outputDiv = document.getElementById('insights-output');
+    outputDiv.innerHTML = "<p>Generating insights... (demo)</p>";
+    setTimeout(function() {
+        outputDiv.innerHTML = "<p>Sales insights generated! (demo)</p>";
+    }, 1000);
+});
